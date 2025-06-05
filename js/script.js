ca8fbe26878c5fed08c9bb4e7da32c7c7742ea74
rolindaz <rolinda.zamboni@gmail.com>
2025-06-05 15:20:56 +0200

    Implement lamp toggle functionality with image switch on button click

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log('hi');
+// console.log('hi');
+
+// image change
+
+// select the lamp off img from html and save it in a variable
+
+const mainImg = document.getElementById('lamp_off')
+console.log(mainImg); // log to check if it selected it
+
+// select the lamp on img from html and save it in a variable
+
+const switchedImg = document.getElementById('lamp_on')
+console.log(switchedImg);
+
+// select the button from html and save it in a variable
+
+const switchBtn = document.getElementById('lamp_switch');
+console.log(switchBtn);
+
+// call the add event listener method to change the img
+
+switchBtn.addEventListener('click', function() {
+    console.log('clicked');
+    mainImg.classList.add('d-none');
+    switchedImg.classList.remove('d-none');
+});
