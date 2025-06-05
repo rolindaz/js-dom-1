422a4b6e728267fa69029d278ecace79fe95dc86
rolindaz <rolinda.zamboni@gmail.com>
2025-06-05 16:37:10 +0200

    Add initial HTML, CSS, images, and JavaScript for lamp toggle feature

---
+++ bonus/js/script.js
@@ -0,0 +1,31 @@
+// console.log('hi');
+
+// image change
+
+// select the lamp off img from html and save it in a variable
+
+const mainImg = document.getElementById('lamp_off')
+console.log(mainImg); // log to check if it selected it
+
+// select the lamp on img from html and save it in a variable
+
+const switchedImg = document.getElementById('lamp_on')
+console.log(switchedImg);
+
+// select the button from html and save it in a variable
+
+const switchBtn = document.getElementById('lamp_switch');
+console.log(switchBtn);
+
+// call the add event listener method to change the img
+
+switchBtn.addEventListener('click', function() {
+    mainImg.classList.toggle('d-none');
+    switchedImg.classList.toggle('d-none');
+    if (mainImg.classList.contains('d-none')) {
+        switchBtn.innerHTML = 'Spegni';
+    } else {
+        switchBtn.innerHTML = 'Accendi'
+    };
+});
+
